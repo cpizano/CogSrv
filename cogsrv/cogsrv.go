048fa0b3bb14df8909b52ebf4dead1bd8e43d61c
Carlos Pizano-Uribe <carlos.pizano@gmail.com>
2013-07-28 17:45:14 -0700

    boilerplate go server
    from go appengine tutorial *yawn*

---
+++ cogsrv/cogsrv.go
@@ -0,0 +1,14 @@
+package cogsrv
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func init() {
+    http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "Hello, cog!")
+}
