dd3d16ff64b75264afa78ec371836acb633eefaa
Carlos Pizano-Uribe <carlos.pizano@gmail.com>
2013-07-28 19:23:45 -0700

    feeling my way into go
    basic json response

--- cogsrv/cogsrv.go
+++ cogsrv/cogsrv.go
@@ -1,14 +1,48 @@
 package cogsrv
 
 import (
+    "encoding/json"
     "fmt"
     "net/http"
+    "strconv"
+
+    "appengine"
 )
 
 func init() {
-    http.HandleFunc("/", handler)
+    http.HandleFunc("/", top_handler)
+    http.HandleFunc("/reg", reg_handler)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Hello, cog!")
+func top_handler(w http.ResponseWriter, r *http.Request) {
+    c := appengine.NewContext(r)
+    w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+
+    fmt.Fprintf(w, "cogsrv  (c) 2013 vortex code\n")
+    fmt.Fprintf(w, "appengine app id = %q\n", appengine.AppID(c))
+    fmt.Fprintf(w, "appengine version = %q\n", appengine.VersionID(c))
+
+    name, index := appengine.BackendInstance(c)
+    fmt.Fprintf(w, "appengine backendinstance = %q, %d\n", name, index)
 }
+
+func reg_handler(w http.ResponseWriter, r *http.Request) {
+    id, _ := strconv.Atoi(r.FormValue("id"))
+    // fmt.Fprintf(w, "registering %d\n", id)
+	rm := map[string]int{}
+	rm["clientid"] = id
+	rm["foo"] = 666
+
+	writeJSON(w, rm)
+}
+
+func writeJSON(w http.ResponseWriter, i interface{}) {
+	buf, err := json.Marshal(i)
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "json.Marshal failed: %v", err)
+		return
+	}
+	w.Write(buf)
+}
+
