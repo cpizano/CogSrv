f37626c20447396447fce4ca32b493f0c4e0b9a7
Carlos Pizano <cpu@chromium.org>
2013-08-12 14:06:04 -0700

    refined message for 'req' handler
    also added a test file1.foo handler

--- cogsrv/cogsrv.go
+++ cogsrv/cogsrv.go
@@ -11,6 +11,7 @@ import (
 func init() {
     http.HandleFunc("/", top_handler)
     http.HandleFunc("/reg", reg_handler)
+    http.HandleFunc("/file1.foo", foo_handler)
 }
 
 func top_handler(w http.ResponseWriter, r *http.Request) {
@@ -25,26 +26,31 @@ func top_handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "appengine backendinstance = %q, %d\n", name, index)
 }
 
-func reg_handler(w http.ResponseWriter, r *http.Request) {  
-  
-  type Message struct {
-    ServerId string
-    Update string
-    Version int32
-    Path string
-  }
-  
-  rm := Message{"773377", "http://localhost:8080/file1.txt", 2, "data/file1.txt"}
-	writeJSON(w, rm)
+func reg_handler(w http.ResponseWriter, r *http.Request) {   
+    type Message struct {
+      ServerId string
+      Update string
+      Component string
+      Name string
+      Version int32
+    }
+    
+    rm := Message{"773377", "http://localhost:8080/file1.foo", "foo", "file1.txt", 2}
+    writeJSON(w, rm)
+}
+
+func foo_handler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+    fmt.Fprint(w, "===========================================")
 }
 
 func writeJSON(w http.ResponseWriter, i interface{}) {
-	buf, err := json.Marshal(i)
-	if err != nil {
-		w.WriteHeader(500)
-		fmt.Fprintf(w, "json.Marshal failed: %v", err)
-		return
-	}
-  w.Header().Set("Content-Type", "text/json; charset=utf-8")
-	w.Write(buf)
+    buf, err := json.Marshal(i)
+    if err != nil {
+      w.WriteHeader(500)
+      fmt.Fprintf(w, "json.Marshal failed: %v", err)
+      return
+    }
+    w.Header().Set("Content-Type", "text/json; charset=utf-8")
+    w.Write(buf)
 }
